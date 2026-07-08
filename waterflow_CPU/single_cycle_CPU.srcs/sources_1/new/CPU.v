02430b676f70da71c68fef67fced39e6c8589ded
liuxxliuxx <liuxxliuxx@yeah.net>
2026-07-08 17:31:04 +0800

    IFU改好了，改成了可以等待总线，请求取指的，同时把一些之前的遗留无关文件删掉了

--- waterflow_CPU/single_cycle_CPU.srcs/sources_1/new/CPU.v
+++ waterflow_CPU/single_cycle_CPU.srcs/sources_1/new/CPU.v
@@ -1,36 +1,119 @@
 `include "CPU_def.vh"
-module read_Instr(
-    input        clk,
-    input        rst,
-    input        branch,
-    input        stall,
-    input [31:0] branch_target,
-    output[31:0] pc_cur,
-    output[31:0] instr,
-    output[31:0] nxt_pc
-    );
-    
-    reg[31:0]  pc;
-    wire[31:0] next_pc;
-    
+
+module IFU(
+    input clk,
+    input rst,
+
+    input if_allowin,
+
+    input         redirect_valid,
+    input  [31:0] redirect_pc,
+
+    input         pred_taken,
+    input  [31:0] pred_target,
+
+    output        if_valid,
+    output [31:0] if_pc,
+    output [31:0] if_pc4,
+    output [31:0] if_inst,
+    output        if_err,
+
+    output        inst_req_valid,
+    input         inst_req_ready,
+    output [31:0] inst_req_vaddr,
+
+    input         inst_resp_valid,
+    input  [31:0] inst_resp_data,
+    input         inst_resp_err
+);
+    reg [31:0] fetch_pc;
+    reg        req_pending;
+    reg [31:0] req_pc_hold;
+
+    reg req_kill;
+    reg out_valid;
+    reg [31:0] out_pc;
+    reg [31:0] out_inst;
+    reg out_err;
+
+    wire[31:0] out_pc4 = out_pc + 32'd4;
+
+    wire out_fire = out_valid && if_allowin;
+
+    wire[31:0] pred_next_pc = pred_taken ? pred_target : out_pc4;
+
+    wire can_issue = !req_pending && (!out_valid ||)
+
+    wire [31:0] issue_pc =
+        redirect_valid ? redirect_pc :
+        (out_valid && if_allowin) ? pred_next_pc :
+                                    fetch_pc;
+
+    assign inst_req_valid = can_issue;
+    assign inst_req_vaddr = issue_pc;
+
+    wire req_fire = inst_req_valid && inst_req_ready;
+
+    assign if_valid = out_valid;
+    assign if_pc    = out_pc;
+    assign if_pc4   = out_pc4;
+    assign if_inst  = out_inst;
+    assign if_err   = out_err;
+
     always @(posedge clk or negedge rst) begin
-        if(!rst)          pc <= 32'h1C000000;
-        else if(!stall)   pc <= next_pc;
+        if (!rst) begin
+            fetch_pc    <= 32'h1C000000;
+
+            req_pending <= 1'b0;
+            req_pc_hold <= 32'b0;
+            req_kill    <= 1'b0;
+
+            out_valid   <= 1'b0;
+            out_pc      <= 32'b0;
+            out_inst    <= 32'b0;
+            out_err     <= 1'b0;
+        end
+        else begin
+            if (redirect_valid) begin
+                fetch_pc  <= redirect_pc;
+                out_valid <= 1'b0;
+                out_pc    <= 32'b0;
+                out_inst  <= 32'b0;
+                out_err   <= 1'b0;
+
+                if (req_pending) begin
+                    req_kill <= 1'b1;
+                end
+            end
+
+            if (out_fire && !redirect_valid) begin
+                out_valid <= 1'b0;
+                fetch_pc  <= pred_next_pc;
+            end
+
+            if (inst_resp_valid && req_pending) begin
+                req_pending <= 1'b0;
+
+                if (req_kill || redirect_valid) begin
+                    req_kill <= 1'b0;
+                end
+                else begin
+                    out_valid <= 1'b1;
+                    out_pc    <= req_pc_hold;
+                    out_inst  <= inst_resp_data;
+                    out_err   <= inst_resp_err;
+                    req_kill  <= 1'b0;
+                end
+            end
+
+            if (req_fire) begin
+                req_pending <= 1'b1;
+                req_pc_hold <= inst_req_vaddr;
+                req_kill    <= 1'b0;
+            end
+        end
     end
-    
-    Instr_rom IR(.addr(pc[17:2]),.data(instr));
-    
-    adder instr_add(
-        .A  (pc),
-        .B  (32'd4),
-        .cin(1'b0),
-        .Sum(nxt_pc),
-        .cout()
-    );
-    
-    assign next_pc = branch ? branch_target : nxt_pc;
-    assign pc_cur  = pc;
-    
+
 endmodule
 
 module Control_Unit(
