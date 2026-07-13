c83054231977e1965ca3593dafd96df1acc8e7e9
liuxxliuxx <liuxxliuxx@yeah.net>
2026-07-13 16:46:37 +0800

    把BPU的预测跳转不再直接和IFU连接并直接取指，WNS从+8.802ns优化到+10.660ns

--- waterflow_CPU/single_cycle_CPU.srcs/sources_1/new/IFU.v
+++ waterflow_CPU/single_cycle_CPU.srcs/sources_1/new/IFU.v
@@ -23,9 +23,13 @@ module IFU(
 
     input         inst_resp_valid,
     input  [31:0] inst_resp_data,
-    input         inst_resp_err
+    input         inst_resp_err,
+
+    output        if_pred_taken,
+    output [31:0] if_pred_target
 );
     reg [31:0] fetch_pc;
+
     reg        redirect_pending;
     reg [31:0] redirect_pc_hold;
     reg        req_pending;
@@ -37,15 +41,18 @@ module IFU(
     reg [31:0] out_inst;
     reg out_err;
 
+    reg        out_pred_taken;
+    reg [31:0] out_pred_target;
+
     wire[31:0] out_pc4 = out_pc + 32'd4;
 
     wire out_fire = out_valid && if_allowin;
 
-    wire[31:0] pred_next_pc = pred_taken ? pred_target : out_pc4;
+    wire[31:0] pred_next_pc = pred_taken ? pred_target : (req_pc_hold + 32'd4);
 
     wire can_issue = !req_pending && (!out_valid || if_allowin);
 
-    wire [31:0] issue_pc = redirect_pending ? redirect_pc_hold : out_fire ? pred_next_pc : fetch_pc;
+    wire [31:0] issue_pc =redirect_pending ? redirect_pc_hold : fetch_pc;
 
     assign inst_req_valid = can_issue && !redirect_valid;
     assign inst_req_vaddr = issue_pc;
@@ -57,6 +64,9 @@ module IFU(
     assign if_pc4   = out_pc4;
     assign if_inst  = out_inst;
     assign if_err   = out_err;
+
+    assign if_pred_taken  = out_pred_taken;
+    assign if_pred_target = out_pred_target;
 
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
@@ -72,6 +82,9 @@ module IFU(
             out_err     <= 1'b0;
             redirect_pending <= 1'b0;
             redirect_pc_hold <= 32'b0;
+
+            out_pred_taken  <= 1'b0;
+            out_pred_target <= 32'b0;
         end
         else begin
             if (redirect_valid) begin
@@ -83,12 +96,14 @@ module IFU(
                 out_inst  <= 32'b0;
                 out_err   <= 1'b0;
 
+                out_pred_taken  <= 1'b0;
+                out_pred_target <= 32'b0;
+
                 req_kill <= req_pending;
             end
 
             if (out_fire && !redirect_valid) begin
                 out_valid <= 1'b0;
-                fetch_pc  <= pred_next_pc;
             end
 
             if (inst_resp_valid && req_pending) begin
@@ -102,6 +117,12 @@ module IFU(
                     out_pc    <= req_pc_hold;
                     out_inst  <= inst_resp_data;
                     out_err   <= inst_resp_err;
+
+                    out_pred_taken  <= pred_taken;
+                    out_pred_target <= pred_target;
+                    fetch_pc        <= pred_next_pc;
+
+
                     req_kill  <= 1'b0;
                 end
             end
